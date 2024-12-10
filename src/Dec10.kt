d8af85778cd25788c1434d3639e9c41d00eb3e5b
shedaniel <daniel@shedaniel.me>
2024-12-10 22:31:54 +0800

    Day 10

---
+++ src/Dec10.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+fun main() {
+    val map = File("src/Dec10.txt").readLines().map { it.toCharArray() }
+
+    fun walkFrom(original: Coord, coord: Coord, visited: MutableSet<Coord>): List<Pair<Coord, Coord>> {
+        return listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1).flatMap { delta ->
+            val new = coord + delta
+            if (new !in visited && new in map && (map[coord].digitToInt() + 1).digitToChar() == map[new]) {
+                if (map[new] == '9') return@flatMap listOf(original to new)
+                val copy = visited.toMutableSet()
+                copy += new
+                return@flatMap walkFrom(original, new, copy)
+            } else {
+                return@flatMap emptyList()
+            }
+        }
+    }
+
+    val sum = mutableSetOf<Pair<Coord, Coord>>()
+    map.forEachMap { coord, c ->
+        if (c == '0') {
+            sum += walkFrom(coord, coord, mutableSetOf())
+        }
+    }
+    println(sum)
+    println(sum.size)
+}
---
+++ src/utils.kt
@@ -0,0 +1,79 @@
+typealias Coord = Pair<Int, Int>
+
+val Coord.x: Int
+    get() = this.first
+
+val Coord.y: Int
+    get() = this.second
+
+operator fun Coord.plus(coord: Coord): Coord {
+    return Coord(this.x + coord.x, this.y + coord.y)
+}
+
+operator fun Coord.minus(coord: Coord): Coord {
+    return Coord(this.x - coord.x, this.y - coord.y)
+}
+
+operator fun Coord.times(scalar: Int): Coord {
+    return Coord(this.x * scalar, this.y * scalar)
+}
+
+operator fun Coord.div(scalar: Int): Coord {
+    return Coord(this.x / scalar, this.y / scalar)
+}
+
+@JvmName("ListArrayContains")
+operator fun <T> List<Array<out T>>.contains(coord: Coord): Boolean {
+    return coord.y in this.indices && coord.x in this[coord.y].indices
+}
+
+@JvmName("ListCharArrayContains")
+operator fun List<CharArray>.contains(coord: Coord): Boolean {
+    return coord.y in this.indices && coord.x in this[coord.y].indices
+}
+
+@JvmName("ListListContains")
+operator fun <T> List<List<T>>.contains(coord: Coord): Boolean {
+    return coord.y in this.indices && coord.x in this[coord.y].indices
+}
+
+@JvmName("ListArrayGet")
+operator fun <T> List<Array<out T>>.get(coord: Coord): T {
+    return this[coord.y][coord.x]
+}
+
+operator fun List<CharArray>.get(coord: Coord): Char {
+    return this[coord.y][coord.x]
+}
+
+@JvmName("ListListGet")
+operator fun <T> List<List<T>>.get(coord: Coord): T {
+    return this[coord.y][coord.x]
+}
+
+@JvmName("ListArrayForEach")
+fun <T> List<Array<out T>>.forEachMap(action: (Coord, T) -> Unit) {
+    for (y in this.indices) {
+        for (x in this[y].indices) {
+            action(x to y, this[y][x])
+        }
+    }
+}
+
+@JvmName("ListCharArrayForEach")
+fun List<CharArray>.forEachMap(action: (Coord, Char) -> Unit) {
+    for (y in this.indices) {
+        for (x in this[y].indices) {
+            action(x to y, this[y][x])
+        }
+    }
+}
+
+@JvmName("ListListForEach")
+fun <T> List<List<T>>.forEachMap(action: (Coord, T) -> Unit) {
+    for (y in this.indices) {
+        for (x in this[y].indices) {
+            action(x to y, this[y][x])
+        }
+    }
+}
